e5e732c6dff6ab967196340ad0e626a74b6a8fd1
Alexandra122 <41281680+Alexandra122@users.noreply.github.com>
2018-08-13 14:31:45 +0300

    task122

---
+++ Task_122/app/src/main/java/com/example/user/task_122/APIinterface.java
@@ -0,0 +1,22 @@
+package com.example.user.task_122;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+interface APIinterface {
+
+    @GET("/api/unknown")
+    Call<MultipleResources>doGetListRecources();
+    @POST("/api/users")
+    Call<User>createUser(@Body User user);
+    @GET("/api/users?")
+    Call<UserList>doGetUserList(@Query("page") String page);
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
